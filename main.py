b9ba6efa4916335741f3e832ac06d5e1ac3b5529
botrott <padgetalex@yandex.ru>
2024-02-20 22:45:06 +0300

    edit readme

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ PASSWORD = os.getenv('PASSWORD')
 
 mailer = SMTPClient(LOGIN, PASSWORD)
 
-mailer.send_mail(emails=['xxx@yandex.ru', 'xxx@gmail.com'],
+mailer.send_mail(emails=['x1@yandex.ru', 'x2@gmail.com'],
                  title='First Test Title',
                  text='This is first SMTP email')
